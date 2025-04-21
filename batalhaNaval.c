52b4097f679d725571ff20d079934766d551a47e
LucasFroner <lucasfroner_@hotmail.com>
2025-04-21 20:42:17 +0000

    Criado o tabuleiro do jogo Batalha Naval e adicionado navios, garantindo que não ultrapasse os limites do tabuleiro e não sobreponha os navios.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -36,5 +36,120 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
+     //declarando variáveis
+     char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+     int linha[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+     int limiteTabuleiroX = 10;
+     int limiteTabuleiroY = 10;
+     int tabuleiro [limiteTabuleiroX] [limiteTabuleiroY];
+     int tamanhoNavioVertical = 3;
+     int tamanhoNavioHorizontal = 3;
+     int inicioYNavioVertical = 2;
+     int posicaoXNavioVertical = 5;
+     int inicioXNavioHorizontal = 6;
+     int posicaoYNavioHorizontal = 6;
+     int navioVertical [tamanhoNavioVertical];
+     int navioHorizontal [tamanhoNavioHorizontal];
+ 
+     //Criando o Tabuleiro
+     printf ("Criando o Tabuleiro\n");
+     printf ("\n"); //quebra de linha para melhor visualização
+ 
+     //Criando o cabeçalho do tabuleiro
+     printf ("   ");
+     for (int y = 0; y < limiteTabuleiroY; y++)
+     {
+         printf ("%c ", coluna[y]);
+     }
+ 
+     printf ("\n"); //quebra de linha para melhor visualização
+ 
+     //Identificação da água do tabuleiro
+     for (int y = 0; y < limiteTabuleiroY; y++)
+     {  
+          //Criando a identificação de linhas
+         if (linha[y] < 10)
+         {
+             printf ("%d  ", linha[y]);
+         } else if (linha [y] >= 10)
+         {
+             printf ("%d ", linha[y]);
+         }
+         
+         for (int x = 0; x < limiteTabuleiroX; x++)
+         {
+             tabuleiro [y] [x] = 0;
+             printf ("%d ", tabuleiro [y] [x]);
+         }
+         printf ("\n");
+     }
+ 
+     //Bloco de verificação de viabilidade de limite do tabuleiro
+     if ( (inicioXNavioHorizontal > limiteTabuleiroX) || 
+         (posicaoYNavioHorizontal > limiteTabuleiroY) || 
+         (inicioXNavioHorizontal + tamanhoNavioHorizontal > limiteTabuleiroX) ) 
+     {
+         printf ("Posição do navio horizontal fora do limite do tabuleiro.\n");
+         return 1;
+     } else if ( (inicioYNavioVertical > limiteTabuleiroY) ||
+                 (posicaoXNavioVertical > limiteTabuleiroX) ||
+                 (inicioYNavioVertical + tamanhoNavioVertical > limiteTabuleiroY) )
+     {
+         printf ("Posição do navio vertical fora do limite do tabuleiro.\n");
+         return 1;
+     }
+ 
+     //Definindo o valor do navio vertical
+     for (int y = inicioYNavioVertical; y < (inicioYNavioVertical + tamanhoNavioVertical); y++)
+     {
+         tabuleiro [y] [posicaoXNavioVertical] = 3;
+     }
+ 
+     //Definindo o valor do navio horizontal
+     for (int x = inicioXNavioHorizontal; x < (inicioXNavioHorizontal + tamanhoNavioHorizontal); x++)
+     {
+         //identificação de colisão de navios
+         if (tabuleiro [posicaoYNavioHorizontal] [x] == 3)
+         {
+             printf ("Erro: Sobreposição de navios.\n");
+             return 1;
+         }
+         
+         tabuleiro [posicaoYNavioHorizontal] [x] = 3;
+     }
+     
+     //Mostrando o Tabuleiro com navios
+     printf ("\n"); //quebra de linha para melhor visualização
+     printf ("Tabuleiro com os navios\n");
+     printf ("\n"); //quebra de linha para melhor visualização
+ 
+         //Criando o cabeçalho do tabuleiro
+     printf ("   ");
+     for (int y = 0; y < limiteTabuleiroY; y++)
+     {
+         printf ("%c ", coluna[y]);
+     }
+ 
+     printf ("\n"); //quebra de linha para melhor visualização
+ 
+     //Mostrar o tabuleiro com navios
+     for (int y = 0; y < limiteTabuleiroY; y++)
+     {
+         //Criando a identificação de linhas
+         if (linha[y] < 10)
+         {
+             printf ("%d  ", linha[y]);
+         } else if (linha [y] >= 10)
+         {
+             printf ("%d ", linha[y]);
+         }
+         for (int x = 0; x < limiteTabuleiroX; x++)
+         {
+             printf ("%d ", tabuleiro [y] [x]);
+         }
+         printf ("\n");
+     }
+
+
     return 0;
 }
